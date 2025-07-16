e0f8759d111053ebc8089d76d74a08df57d4abe3
shaswatkr19 <shaswatsinha000@gmail.com>
2025-07-16 16:48:31 +0530

    Initial commit of PyroIQBot

---
+++ main.py
@@ -0,0 +1,72 @@
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+#create openai client
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+#/Start command
+async def start(update, context):
+    intro_text = (
+        "👋 Yo! Main ChatGPT se live connected hoon 💬 tech ka jaadu mere paas hai! Kuch bhi puchh, har sawaal ka jawab milega 😎🔥🤖\n\n"
+        "🔹Try sending anything like:\n"
+        "What is Python?\n"
+        "Tell me a joke\n"
+        "How to learn coding?\n\n"
+        "Type /stop when you're done. Let's go! 🚀"
+    )
+    await update.message.reply_text(intro_text)
+
+# /stop handler
+async def stop(update, context):
+    goodbye_text = (
+        "🙏 Thank you for chatting with me!\n"
+        "Hope I helped you today 🌟\n"
+        "You can come back anytime by typing /start 💬\n\n"
+        "Have a great day ahead! 🌈"
+    )
+    await update.message.reply_text(goodbye_text)
+
+#Handle normal text messages
+async def handle_message(update, context):
+    user_msg = update.message.text
+
+    # 👋 Auto greet if first message is casual
+    if user_msg.lower() in ["hi", "hello", "hey", "start", "yo"]:
+        intro_text = (
+            "👋 Yo! Main ChatGPT se live connected hoon 💬 tech ka jaadu mere paas hai! Kuch bhi puchh, har sawaal ka jawab milega 😎🔥🤖\n\n"
+            "🔹 Try sending anything like:\n"
+            "What is Python?\n"
+            "Tell me a joke\n"
+            "How to learn coding?\n\n"
+            "Type /stop when you're done. Let's go! 🚀"
+        )
+        await update.message.reply_text(intro_text)
+        return
+
+    try:
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo", 
+            messages=[{"role": "user", "content": user_msg}],
+        )
+
+        bot_reply = response.choices[0].message.content
+        await update.message.reply_text(bot_reply)
+
+    except Exception as e:
+        await update.message.reply_text(f"Oops! System thoda tilt ho gaya 😵‍💫⚙️\nError ka scene hai: {e}")    
+
+
+#Setup bot
+app = ApplicationBuilder().token(BOT_TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CommandHandler("Stop", stop))
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+
+print("Bot connected with OpenAI and running....")
+app.run_polling()
